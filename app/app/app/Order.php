5ec0382be48067743ef8a63f6590945e9dd89b00
matijavavetic <47482084+matijavavetic@users.noreply.github.com>
2019-06-01 19:13:17 +0200

    Testing eloquent

---
+++ app/app/app/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use App\Product;
+
+class Order
+{
+    protected $products = [];
+
+    public function add(Product $product)
+    {
+        $this->products[] =  $product;
+    }
+
+    public function products()
+    {
+        return $this->products;
+    }
+
+    public function total()
+    {
+        return array_reduce($this->products, function($carry, $product) {
+            return $carry + $product->cost();
+        });
+    }
+}
